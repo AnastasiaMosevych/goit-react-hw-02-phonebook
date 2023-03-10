809a8a114bea33a86703445a65de6dcbcea0e212
AnastasiaMosevych <ukrainischesmaedel@yahoo.de>
2023-03-10 23:14:04 +0100

    commented some more unused code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { FormStyle, Label, Input, SubmitBtn } from './App.styled';
 
 export class App extends Component {
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import { useFormik } from 'formik';
+// import { Component } from 'react';
+// import { useFormik } from 'formik';
 
 // export class PhonebookForm extends Component {
 //     const formik = useFormik ({
