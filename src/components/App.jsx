da5640c50ba0d6417e196f8da29b65e3f2137c8b
AnastasiaMosevych <ukrainischesmaedel@yahoo.de>
2023-03-10 23:07:18 +0100

    completed step 1 of task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from "react";
+import { nanoid } from 'nanoid';
+import { FormStyle, Label, Input, SubmitBtn } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+  render() {
+    return (
+      <>
+      <FormStyle>
+        <div>
+          <Label>Name</Label>
+          <Input type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required  
+          >
+          </Input>
+        </div>
+        <SubmitBtn type="submit">Add contact</SubmitBtn>
+        </FormStyle>
+        </>
   );
+  }
 };
