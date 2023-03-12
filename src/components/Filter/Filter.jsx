90e1520405d00be893056969fa7712f03044a4e4
AnastasiaMosevych <ukrainischesmaedel@yahoo.de>
2023-03-12 22:04:21 +0100

    added forgotten proptypes to filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,13 @@
-import { Input, Label } from "components/Phonebook/Phonebook.styled"
+import { Input, Label } from "components/Phonebook/Phonebook.styled";
+import PropTypes from 'prop-types';
 
 export const Filter = ({updateFilter}) => {
     return <>
         <Label>Find contacts by name</Label>
         <Input onChange={updateFilter} type="search" name="filter"></Input>
     </>
+}
+
+Filter.propTypes = {
+    updateFilter: PropTypes.func.isRequired,
 }
